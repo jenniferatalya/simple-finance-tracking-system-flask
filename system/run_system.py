e92388c0e67a023ee741414c059ed05f96abba34
Bebez <jsumantri41@students.calvin.ac.id>
2022-10-17 14:43:02 +0700

    Update Sistem Login

--- system/run_system.py
+++ system/run_system.py
@@ -1,4 +1,4 @@
-from app import db, app
+from app import db, app, User, Role
 
 
 class DBSystem:
@@ -9,7 +9,23 @@ class DBSystem:
         with app.app_context():
             self.db.create_all()
 
+    def delete_db(self):
+        with app.app_context():
+            self.db.drop_all()
+
+    def log_in(self, username, password):
+        with app.app_context():
+            user_ = User.query.where(User.user_id == username).all()
+            if len(user_) != 0:
+                user_id_ = user_[0]
+                password_in = user_id_.user_pswd
+                if password_in == password:
+                    role_id_ = user_id_.role_id
+                    role_ = Role.query.where(Role.role_id == role_id_).all()
+                    return role_[0].role_name
+
 
 if __name__ == "__main__":
     dbsistem = DBSystem(db)
-    dbsistem.create_db()
+    print(dbsistem.log_in('141', 'akuganteng'))
+    # dbsistem.delete_db()
