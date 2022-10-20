82a6f5815c8aeac16982f36fcfb2c28cafcd38de
renatavalencia <rntvlnc@gmail.com>
2022-10-20 15:17:34 +0700

    Payment Frontend System

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='assets')
+app.secret_key = 'TinyJar Production'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root@127.0.0.1/tiny_jar'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
